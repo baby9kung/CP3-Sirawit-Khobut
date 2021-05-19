1a67a46a7566adda80bdbf8db2d270bfc528b701
baby9kung <babyninekung@gmail.com>
2021-05-19 11:58:46 +0700

    Create Exercise 5.2

---
+++ Exercise5_2_Sirawit_K.py
@@ -0,0 +1,8 @@
+print("---------------------")
+s = float(input("DISTANCE (KM) :"))
+t = float(input("TIME (H) :"))
+v=s/t
+print("---------------------")
+print("RESULT :",v,"Km/h")
+print("---------------------")
+
