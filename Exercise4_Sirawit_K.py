3a7685e4b519e5127b046db4b00671edab4b37c8
baby9kung <babyninekung@gmail.com>
2021-05-19 09:24:43 +0700

    Create Exercise 4

---
+++ Exercise4_Sirawit_K.py
@@ -0,0 +1,9 @@
+FENG = 80
+GBUS = 84
+ICS = 92
+CP = 99
+print("---Your Score---")
+print("Foundation English   : ",FENG)
+print("General Business     : ",GBUS)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming : ",CP)
