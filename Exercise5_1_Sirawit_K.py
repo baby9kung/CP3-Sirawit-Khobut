0f0a4884b81327af12127c4e8b899f4b2f073f76
baby9kung <babyninekung@gmail.com>
2021-05-19 11:58:36 +0700

    Create Exercise 5.1

---
+++ Exercise5_1_Sirawit_K.py
@@ -0,0 +1,8 @@
+x = float(input("NUMBER :"))
+y = float(input("NUMBER :"))
+print("---------------------")
+print(x,"+",y,"=",x+y)
+print(x,"-",y,"=",x-y)
+print(x,"*",y,"=",x*y)
+print(x,"/",y,"=",x/y)
+print("---------------------")
